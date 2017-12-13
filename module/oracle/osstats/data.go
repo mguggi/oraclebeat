4df7c47628947d6f8ae460d7007acecf3e797d33
mguggi <manuel.guggi@infonova.com>
2017-12-13 11:46:53 +0100

    add metricset osstats

---
+++ module/oracle/osstats/data.go
@@ -0,0 +1,25 @@
+package osstats
+
+import (
+	s "github.com/elastic/beats/libbeat/common/schema"
+	c "github.com/elastic/beats/libbeat/common/schema/mapstrstr"
+)
+
+// Based on https://docs.oracle.com/en/database/oracle/oracle-database/12.2/refrn/V-OSSTAT.html#GUID-E1E48692-47FA-4AE3-9402-82477E66FFC0
+var (
+	schema = s.Schema{
+		"database": s.Object{
+			"container": s.Object{
+				"id": c.Int("CON_ID", s.Optional),
+				"instance": s.Object{
+					"id": c.Int("INST_ID"),
+				},
+			},
+		},
+		"id":         c.Int("OSSTAT_ID"),
+		"name":       c.Str("STAT_NAME"),
+		"value":      c.Int("VALUE"),
+		"comments":   c.Str("COMMENTS"),
+		"cumulative": c.Str("CUMULATIVE"),
+	}
+)
