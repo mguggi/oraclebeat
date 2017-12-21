47c8e22ef42029507bc5e0bb5814082abddbe7dc
mguggi <manuel.guggi@infonova.com>
2017-12-21 13:54:26 +0100

    move binary file after download

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # vim: set et sw=2 ts=2:
 
-ORACLE_DOWNLOAD_DIR="dockerfiles/11.2.0.2"
+ORACLE_DOWNLOAD_DIR="oracle11g/xe"
 ORACLE_DOWNLOAD_FILE="oracle-xe-11.2.0-1.0.x86_64.rpm.zip"
 
 if [ -n "$ORACLE_DOWNLOAD_DIR" ]; then
@@ -26,3 +26,5 @@ echo > "$COOKIES"
 chmod 600 "$COOKIES"
 
 exec node download.js
+
+mv $ORACLE_DOWNLOAD_FILE dockerfiles/11.2.0.2
